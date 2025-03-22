63b1e308d85924c5deaeed2ed8e0f2ad12a56d3a
zeroteq <99808976+zeroteq@users.noreply.github.com>
2025-03-22 22:38:02 +0200

    Add files via upload

---
+++ models/UserLike.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userLikeSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    listingId: mongoose.Schema.Types.ObjectId
+});
+
+module.exports = mongoose.model('UserLike', userLikeSchema);
