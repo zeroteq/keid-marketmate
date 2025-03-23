c46511e96912b0c07650a8ab21e8bd188bba7bdf
zeroteq <99808976+zeroteq@users.noreply.github.com>
2025-03-23 13:20:19 +0200

    Delete models directory

--- models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const favoriteSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    listingId: mongoose.Schema.Types.ObjectId
-});
-
-module.exports = mongoose.model('Favorite', favoriteSchema);
