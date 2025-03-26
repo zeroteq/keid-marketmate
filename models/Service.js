fce0a7ca165b71d151d83a1806e239fed078aa6d
zeroteq <99808976+zeroteq@users.noreply.github.com>
2025-03-26 14:05:35 +0200

    Add files via upload

--- models/Service.js
+++ models/Service.js
@@ -9,6 +9,8 @@ const serviceSchema = new mongoose.Schema({
     images: [String],
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     likes: { type: Number, default: 0 },
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    favoritedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     createdAt: { type: Date, default: Date.now },
     category: String
 });
