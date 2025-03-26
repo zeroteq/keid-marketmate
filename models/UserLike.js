d9937bc5e8b6d39548424b58a1afa3408629c89f
zeroteq <99808976+zeroteq@users.noreply.github.com>
2025-03-26 15:45:05 +0200

    Delete models directory

--- models/UserLike.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userLikeSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    listingId: mongoose.Schema.Types.ObjectId,
-    profileId: mongoose.Schema.Types.ObjectId,
-});
-
-module.exports = mongoose.model('UserLike', userLikeSchema);
